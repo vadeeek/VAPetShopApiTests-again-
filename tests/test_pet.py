90830c0eb4c729b14c83fdc95de9f5c6e42b0412
Vadim Velichko <super.velik.123@mail.ru>
2025-11-03 02:51:59 +0300

    Небольшие фиксы

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,14 +1,14 @@
 import allure
 import requests
 
-BASE_URL = "https://petstore.swagger.io/v2"
+BASE_URL = "http://5.181.109.28:9090/api/v3"
 
 @allure.feature("Pet")
 class TestPet:
     @allure.title("Попытка удалить несуществующего питомца")
     def test_delete_nonexistent_pet(self):
         with allure.step("Отправка запроса на удаление несуществующего питомца"):
-            response = requests.delete(url=f'{BASE_URL}/pet/10')
+            response = requests.delete(url=f'{BASE_URL}/pet/9999')
 
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
