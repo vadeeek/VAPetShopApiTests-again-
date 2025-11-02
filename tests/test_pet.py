02fbfd6fce1e53286a9afdf4013175b34e24d8a6
Vadim Velichko <super.velik.123@mail.ru>
2025-11-03 02:45:17 +0300

    Добавил тест "Попытка удалить несуществующего питомца"

---
+++ tests/test_pet.py
@@ -0,0 +1,17 @@
+import allure
+import requests
+
+BASE_URL = "https://petstore.swagger.io/v2"
+
+@allure.feature("Pet")
+class TestPet:
+    @allure.title("Попытка удалить несуществующего питомца")
+    def test_delete_nonexistent_pet(self):
+        with allure.step("Отправка запроса на удаление несуществующего питомца"):
+            response = requests.delete(url=f'{BASE_URL}/pet/10')
+
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка текстового содержимого ответа"):
+            assert response.text == "Pet deleted", "Текст ошибки не совпал с ожидаемым"
