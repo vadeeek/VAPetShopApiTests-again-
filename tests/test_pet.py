f01b927b42e035eaccfd4ae46e73cce5542fd095
Vadim Velichko <super.velik.123@mail.ru>
2026-03-05 21:45:24 +0300

    Добавил тест "Добавление нового питомца с полными данными"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -64,3 +64,32 @@ class TestPet:
             assert response_json['name'] == payload['name'], "name питомца не совпадает с ожидаемым"
             assert response_json['status'] == payload['status'], "status питомца не совпадает с ожидаемым"
 
+    @allure.title("Добавление нового питомца с полными данными")
+    def test_add_full_pet(self):
+        payload = {
+            "id": 10,
+            "name": "doggie",
+            "category": {
+                "id": 1,
+                "name": "Dogs"
+            },
+            "photoUrls": ["string"],
+            "tags": [{"id": 0, "name": "string"}],
+            "status": "available"
+        }
+
+        with allure.step("Отправка запроса на создание нового питомца с полными данными"):
+            response = requests.post(f'{BASE_URL}/pet', json=payload)
+            response_json = response.json()
+
+        with allure.step("Проверка статуса ответа и валидации JSON-схемы"):
+            assert response.status_code == 200, "Код ответа не совпал с ожидаемым"
+            validate(response_json, PET_SCHEMA)
+
+        with allure.step("Проверка параметров питомца в ответе"):
+            assert response_json['id'] == payload['id'], "id питомца не совпал с ожидаемым"
+            assert response_json['name'] == payload['name'], "name питомца не совпал с ожидаемым"
+            assert response_json['category'] == payload['category'], "category питомца не совпал с ожидаемым"
+            assert response_json['photoUrls'] == payload['photoUrls'], "photoUrls питомца не совпал с ожидаемым"
+            assert response_json['tags'] == payload['tags'], "tags питомца не совпал с ожидаемым"
+            assert response_json['status'] == payload['status'], "status питомца не совпал с ожидаемым"
